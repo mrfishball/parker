f142b67bb36f618216645153cd8df3d3682319f3
Steven Tuet Piu Kwok <mrfishball@gmail.com>
2020-09-24 13:08:37 -0400

    Fix failing tests

--- spec/requests/ticket_request_spec.rb
+++ spec/requests/ticket_request_spec.rb
@@ -1,19 +1,19 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Tickets", type: :request do
-
-  describe "GET /index" do
-    it "returns http success" do
-      get "/ticket/index"
+RSpec.describe 'Tickets', type: :request do
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/ticket/index'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/ticket/create"
+  describe 'GET /show' do
+    it 'returns http success' do
+      get '/ticket/show'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
